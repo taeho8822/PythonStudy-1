e87de8d3afc5ed23b650159e1d4d48afe0a2f4ea
dobbyisfree8282@gmail.com <zahir7@naver.com>
2023-06-09 21:57:03 +0900

    1

--- Day05/Section12/Ex12-10-external.py
+++ Day05/Section12/Ex12-10-external.py
@@ -12,4 +12,17 @@ console> pip uninstall package명
 '''
 import numpy as np
 
-print(np.sum([1,2,3,4,5]))
+
+# 1차원 배열 생성
+arr1 = np.array([1, 2, 3, 4, 5])
+
+# 2차원 배열 생성
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+# 배열의 크기 확인
+print(arr1.shape)  # 출력: (5,)
+print(arr2.shape)  # 출력: (2, 3)
+
+# 배열 연산
+result = arr1 + 10
+print(result)  # 출력: [11, 12, 13, 14, 15]
